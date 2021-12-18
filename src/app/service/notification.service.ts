a064f89c277915aa95b70e02b15418dbc6b28661
Dani Setiawan <mayadani28@gmail.com>
2021-12-19 00:49:34 +0700

    update

--- src/app/service/notification.service.ts
+++ src/app/service/notification.service.ts
@@ -1,3 +1,6 @@
+import { Injectable } from "@angular/core";
+import { NotifierService } from "angular-notifier";
+
 @Injectable({ providedIn: 'root' })
 export class NotificationService {
   private readonly notifier: NotifierService;
